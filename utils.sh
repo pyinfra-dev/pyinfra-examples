fbbebc2439c410356c504188da148ce3cd47379c
Nick Mills-Barrett <nick@fizzadar.com>
2024-05-12 21:25:38 +0100

    Add Docker systemd test container & utils

---
+++ utils.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOCKER_TEST_IMAGE_NAME="pyinfra-examples-docker-test-image"
+DOCKER_TEST_NETWORK_NAME="pyinfra-examples-docker-test-network"
+
+function ensure_test_container() {
+    docker image inspect $DOCKER_TEST_IMAGE_NAME > /dev/null 2>&1 && return 0
+    docker_dir=$(realpath "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/docker")
+    cd "$docker_dir" || return 1
+    docker build -f Dockerfile-test-server -t $DOCKER_TEST_IMAGE_NAME .
+}
+
+function run_test_container() {
+    docker run -d "${@:2}" \
+        --network "$DOCKER_TEST_NETWORK_NAME" \
+        --name "$1" \
+        --volume /sys/fs/cgroup:/sys/fs/cgroup \
+        --tmpfs /run \
+        --tmpfs /run/lock \
+        --tmpfs /tmp \
+        "$DOCKER_TEST_IMAGE_NAME"
+}
