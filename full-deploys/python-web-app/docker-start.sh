5ff73b7fe6911c62e15eb8a7c68a75c0ef291f06
Nick Mills-Barrett <nick@fizzadar.com>
2026-02-03 19:07:59 +0000

    Use better name for python app deploy file

--- full-deploys/python-web-app/docker-start.sh
+++ full-deploys/python-web-app/docker-start.sh
@@ -17,6 +17,6 @@ run_test_container pyinfra-example-python-web-app-dbserver -p 9023:22
 echo
 echo "Doker containers are now ready to run the pyinfra deploy, you can do this by running:"
 echo
-echo "    pyinfra inventories/docker.py deploy.py"
+echo "    pyinfra inventories/docker.py deploy_app.py"
 echo
 echo "Once complete, don't forget to remove the Docker containers and network using the ./docker-stop.sh script!"
